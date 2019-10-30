9660d1b4ff7804baf7ffdb345a3311487547fc15
Maruko <marco.k95@hotmail.com>
2019-10-29 21:00:12 -0300

    fix logic

--- src/main/kotlin/com/boltChallenge/demo/AppController.kt
+++ src/main/kotlin/com/boltChallenge/demo/AppController.kt
@@ -16,33 +16,36 @@ class AppController {
     }
 
     @PostMapping
-    fun add(@RequestBody input:List<Input>):List<Input>{
+    fun add(@RequestBody input:List<Input>):List<String>{
 
-        inputRepository.deleteAll()
+        //inputRepository.deleteAll()
         var opCount:Int
         var inputs = arrayListOf<Input>()
+        var strs:Input = input.get(0)
 
-        for(i in 0 until input.size){
+        for(i in 0 until input.size) {
             opCount = 0
 
-            if(input[i].str.length % 2 == 0){
-                for(j in 0 until input[i].str.length step 2){
+            if (input[i].str.length % 2 == 0) {
+                for (j in 0 until input[i].str.length step 2) {
 
-                    if( input[i].str.get(j).equals('{') && input[i].str.get(j+1).equals('{') ) {
-                        opCount+= 1
+                    if (input[i].str.get(j).equals('{') && input[i].str.get(j + 1).equals('{')) {
+                        opCount += 1
                     }
-                    if( input[i].str.get(j).equals('}') && input[i].str.get(j+1).equals('}') ) {
-                        opCount+= 1
+                    if (input[i].str.get(j).equals('}') && input[i].str.get(j + 1).equals('}')) {
+                        opCount += 1
                     }
-                    if( input[i].str.get(j).equals('}') && input[i].str.get(j+1).equals('{') ) {
-                        opCount+= 2
+                    if (input[i].str.get(j).equals('}') && input[i].str.get(j + 1).equals('{')) {
+                        opCount += 2
                     }
 
                 }
+                input[i].changeResult(input[i].str)
                 input[i].changeStr(opCount.toString())
                 inputRepository.save(input[i])
                 inputs.add(input[i])
-            }else{
+            } else {
+                input[i].changeResult(input[i].str)
                 input[i].changeStr("não pode ser estável, número Ímpar de entrada")
                 inputRepository.save(input[i])
                 inputs.add(input[i])
@@ -50,7 +53,7 @@ class AppController {
 
         }
 
-        return inputs
+        return strs.returnStrs(inputs)
 
     }
 
--- src/main/kotlin/com/boltChallenge/demo/Input.kt
+++ src/main/kotlin/com/boltChallenge/demo/Input.kt
@@ -8,10 +8,27 @@ import javax.persistence.Id
 class Input(@Id
             @GeneratedValue
             val id: Long = 0,
-            var str:String = ""){
+            var str:String = "",
+            var result:String = ""){
 
     fun changeStr (str:String){
         this.str = str
+    }
+
+    fun changeResult (result:String){
+        this.result = result
+    }
+
+    fun returnStrs(inputs:List<Input>):List<String>{
+
+        var strs = arrayListOf<String>()
+
+        for(input in inputs){
+            strs.add(input.str)
+        }
+
+        return strs
+
     }
 
 }
